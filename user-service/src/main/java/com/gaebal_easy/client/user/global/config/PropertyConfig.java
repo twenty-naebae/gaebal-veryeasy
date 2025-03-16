622dc52d8b1bbbee8bebd29bb2e7a8ed72fa1616
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-03-16 14:36:12 +0900

    chor : user-service에서 common 모듈 의존성 추가

--- user-service/src/main/java/com/gaebal_easy/client/user/global/config/PropertyConfig.java
+++
@@ -1,9 +0,0 @@
-package com.gaebal_easy.client.user.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource(value = "classpath:/properties/env.properties", ignoreResourceNotFound = true) // 파일이 없으면 무시
-public class PropertyConfig {
-}
