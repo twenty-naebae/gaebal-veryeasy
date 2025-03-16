af70d4487d9ea8a085d5748d54a36869667bcdd2
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-03-16 22:49:40 +0900

    feat : 회원가입 - Repository 구현

--- user-service/src/main/java/com/gaebal_easy/client/user/domain/repository/UserRepository.java
+++ user-service/src/main/java/com/gaebal_easy/client/user/domain/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.gaebal_easy.client.user.domain.repository;
 
+import com.gaebal_easy.client.user.domain.entity.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface UserRepository {
+    public User save(User user);
 }
---
+++ user-service/src/main/java/com/gaebal_easy/client/user/infrastructure/UserJpaRepository.java
@@ -0,0 +1,7 @@
+package com.gaebal_easy.client.user.infrastructure;
+
+import com.gaebal_easy.client.user.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserJpaRepository extends JpaRepository<User, Long>{
+}
