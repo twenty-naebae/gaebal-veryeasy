e72a7741257cc5d9affac26e7dd995ae9b3e5f10
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-03-16 16:15:19 +0900

    fix : 빌드가 정상적으로 되도록 하기 위해 common 모듈을 수정
    - 기존걸 복사하는 과정에서 패키지 명이 잘못되어있어 수정하였습니다.
    - common모듈에 필요한 의존성을 추가하였습니다.
    - Eureka서비스에 정상적으로 user-service가 등록되기 위하여 build.gradle을 수정하였습니다.

--- common/src/main/java/gaebal_easy/common/global/exception/Code.java
+++ common/src/main/java/gaebal_easy/common/global/exception/Code.java
@@ -1,10 +1,11 @@
-package com.gaebal_easy.client.user.global.exception;
+package gaebal_easy.common.global.exception;
 
-import java.util.Optional;
-import java.util.function.Predicate;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+
+import java.util.Optional;
+import java.util.function.Predicate;
 
 @Getter
 @RequiredArgsConstructor
@@ -16,50 +17,49 @@ public enum Code {
     SUCCESS(HttpStatus.OK, 0, "성공적으로 처리되었습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,1,"서버에러"),
     /**
-     * Gate-way 10000번대
+     * Gate-way 1000번대
      */
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST, 10000, "잘못된 입력값이 존재합니다.");
 
     /**
-     * Eureak 20000번대
+     * Eureak 2000번대
      */
 
     /**
-     * User 30000번대
+     * User 3000번대
      */
 
     /**
-     * Hub 40000번대
+     * Hub 4000번대
      */
 
     /**
-     * Delivery 50000번대
+     * Delivery 5000번대
      */
 
     /**
-     * Product 60000번대
+     * Product 6000번대
      */
 
     /**
-     * Order 80000번대
+     * Order 7000번대
      */
 
     /**
-     * Store 90000번대
+     * Store 8000번대
      */
 
     /**
-     * Slcak 100000번대
+     * Slcak 9000번대
      */
 
     /**
-     * Kafka 통신 110000번대
+     * Kafka 통신 10000번대
      */
 
     /**
-     * feighn Client 통신 120000번대
+     * feighn Client 통신 11000번대
      */
-
 
     private final HttpStatus status;
     private final Integer code;
