5ad028858a1d9c4dd6404c9314b6a4167597a818
kdh610 <k_dh610@naver.com>
2025-03-17 17:42:20 +0900

    feat: Product Entity 생성

--- product-service/src/main/java/com/gaebal_easy/product/infrastructure/config/GlobalConfig.java
+++ product-service/src/main/java/com/gaebal_easy/product/infrastructure/config/GlobalConfig.java
@@ -1,12 +1,14 @@
 package com.gaebal_easy.product.infrastructure.config;
 
 import gaebal_easy.common.global.config.PropertyConfig;
+import gaebal_easy.common.global.entity.BaseTimeEntity;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
-        PropertyConfig.class
+        PropertyConfig.class,
+        BaseTimeEntity.class
 })
 public class GlobalConfig {
 }
