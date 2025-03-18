8b03060b73791b9fb92e8c6107cc84cec8482bc2
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-03-18 23:18:48 +0900

    feat : hub-service에 테스트 컨트롤러 추가

---
+++ hub-service/src/main/java/com/gaebal_easy/hub/infrastructure/config/GlobalConfig.java
@@ -0,0 +1,18 @@
+package com.gaebal_easy.hub.infrastructure.config;
+
+import gaebal_easy.common.global.config.JpaAuditingConfig;
+import gaebal_easy.common.global.config.PropertyConfig;
+import gaebal_easy.common.global.security.GlobalSecurityConfig;
+import gaebal_easy.common.global.security.GlobalSecurityContextFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({
+        JpaAuditingConfig.class,
+        PropertyConfig.class,
+        GlobalSecurityConfig.class,
+        GlobalSecurityContextFilter.class
+})
+public class GlobalConfig {
+}
---
+++ hub-service/src/main/java/com/gaebal_easy/hub/presentation/TestController.java
@@ -0,0 +1,15 @@
+package com.gaebal_easy.hub.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hub-service/api")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
