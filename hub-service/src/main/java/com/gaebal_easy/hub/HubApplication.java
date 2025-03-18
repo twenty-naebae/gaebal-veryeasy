447268541136401b2cd0047f9015833672ce4955
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-03-18 23:18:48 +0900

    feat : hub-service에 테스트 컨트롤러 추가

--- hub-service/src/main/java/com/gaebal_easy/hub/HubApplication.java
+++ hub-service/src/main/java/com/gaebal_easy/hub/HubApplication.java
@@ -2,6 +2,7 @@ package com.gaebal_easy.hub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
