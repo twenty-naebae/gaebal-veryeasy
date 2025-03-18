e94fa5f921a137fc844553150dce073fa03c4eca
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-03-18 11:46:26 +0900

    refactor : 테이블 명 통일, 권한 수정
    - 테이블명에 p_ 접두어를 추가하였습니다.
    - 배송담당자 role을 세분화 하였고, 통일성있게 권한 이름을 수정하였습니다.

--- common/src/main/java/gaebal_easy/common/global/enums/Role.java
+++ common/src/main/java/gaebal_easy/common/global/enums/Role.java
@@ -9,8 +9,9 @@ public enum Role {
 
     MASTER("ROLE_MASTER"), //마스터
     HUB_MANAGER("ROLE_HUB_MANAGER"), //허브관리자
-    DELIVERY_USER("ROLE_DELIVERY_USER"), //배송 담당자
-    COMPANY_USER("ROLE_COMPANY_USER"); //업체 담당자
+    HUB_DELIVERY_USER("ROLE_HUB_DELIVERY_USER"), // 허브 배송 담당자
+    STORE_DELIVERY_USER("ROLE_STORE_DELIVERY_USER"), // 업체 배송 담당자
+    STORE_MANAGER("ROLE_STORE"); //업체 담당자
 
     private final String roleName;
 
