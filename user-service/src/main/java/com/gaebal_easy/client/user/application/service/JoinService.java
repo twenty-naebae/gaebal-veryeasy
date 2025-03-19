b4d3d203ca94e9d735f1791da3999cccc459dc01
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-03-19 16:09:22 +0900

    feat : 회원가입 - 토픽 메세지 발행
    - hub-service에서 consumer 작성 예정

--- user-service/src/main/java/com/gaebal_easy/client/user/application/service/JoinService.java
+++ user-service/src/main/java/com/gaebal_easy/client/user/application/service/JoinService.java
@@ -1,8 +1,10 @@
 package com.gaebal_easy.client.user.application.service;
 
+import com.gaebal_easy.client.user.application.dto.HubManagerInfoMessage;
 import com.gaebal_easy.client.user.domain.entity.User;
 import com.gaebal_easy.client.user.domain.repository.UserRepository;
 import com.gaebal_easy.client.user.presentation.dto.JoinRequest;
+import gaebal_easy.common.global.enums.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,12 +16,19 @@ public class JoinService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ProducerService producerService;
 
     @Transactional
     public void join(JoinRequest joinRequest) {
         joinRequest.setPassword(bCryptPasswordEncoder.encode(joinRequest.getPassword()));
         User user = new User(joinRequest);
-        //todo - 각 서비스에 권한에 맞는 유저 생성 이벤트 요청
         userRepository.save(user);
+        //todo - 각 서비스에 권한에 맞는 유저 생성 이벤트 요청
+        if(user.getRole().equals(Role.HUB_MANAGER)){
+            producerService.sendHubManagerInfo(HubManagerInfoMessage.of(user.getId(), joinRequest.getName(), joinRequest.getGroup()));
+        }
+        else if(user.getRole().equals(Role.HUB_DELIVERY_USER)|| user.getRole().equals(Role.STORE_DELIVERY_USER)){
+            //todo - sendDeliverUserInfo 처리
+        }
     }
 }
