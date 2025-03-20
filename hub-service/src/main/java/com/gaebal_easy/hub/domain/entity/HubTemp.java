1df0906d3acc824a2958277f73f04eeae1e821ad
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-03-20 13:00:44 +0900

    feat : 회원가입 -허브매니저 생성로직 작성

--- hub-service/src/main/java/com/gaebal_easy/hub/domain/entity/HubTemp.java
+++ hub-service/src/main/java/com/gaebal_easy/hub/domain/entity/HubTemp.java
@@ -2,6 +2,7 @@ package com.gaebal_easy.hub.domain.entity;
 
 import gaebal_easy.common.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
+import lombok.Getter;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UuidGenerator;
 
@@ -9,11 +10,12 @@ import java.util.UUID;
 
 // 회원가입 테스트하기위해 가짜 Hub Entity입니다.
 @Entity
+@Getter
 @Table(name="p_hub_temp")
 public class HubTemp extends BaseTimeEntity{
 
     @Id
-    @UuidGenerator
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column
     private UUID id;
 
---
+++ hub-service/src/main/java/com/gaebal_easy/hub/domain/repository/HubManagerRepository.java
@@ -0,0 +1,7 @@
+package com.gaebal_easy.hub.domain.repository;
+
+import com.gaebal_easy.hub.domain.entity.HubManager;
+
+public interface HubManagerRepository {
+    public HubManager save(HubManager hubManager);
+}
