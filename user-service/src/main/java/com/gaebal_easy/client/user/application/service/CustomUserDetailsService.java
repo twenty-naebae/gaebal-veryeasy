46a0939d5f89a23d4aefab3530d078fbe774b3a7
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-03-21 14:52:32 +0900

    fix : 로그인 실패시 예외처리중

--- user-service/src/main/java/com/gaebal_easy/client/user/application/service/CustomUserDetailsService.java
+++ user-service/src/main/java/com/gaebal_easy/client/user/application/service/CustomUserDetailsService.java
@@ -8,18 +8,18 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class CustomUserDetailsService implements UserDetailsService {
-//
-//    private final UserRepository userRepository;
-//
-//    // userId를 기준으로 User를 찾아 CustomUserDetails로 반환합니다.
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        User user = userRepository.findByUsername(username);
-//        return new CustomUserDetails(user);
-//    }
-//
-//}
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // userId를 기준으로 User를 찾아 CustomUserDetails로 반환합니다.
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        return new CustomUserDetails(user);
+    }
+
+}
