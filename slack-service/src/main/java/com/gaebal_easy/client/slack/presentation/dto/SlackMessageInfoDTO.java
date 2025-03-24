54c0542a85b87c2ed34d31f9fe736fea3cba5242
pkl0912 <pkl0912@naver.com>
2025-03-24 22:45:54 +0900

    refactor: 슬랙 카프카 연동, gemini 비동기 처리

--- slack-service/src/main/java/com/gaebal_easy/client/slack/presentation/dto/SlackMessageInfoDTO.java
+++ slack-service/src/main/java/com/gaebal_easy/client/slack/presentation/dto/SlackMessageInfoDTO.java
@@ -17,7 +17,6 @@ public class SlackMessageInfoDTO {
 	private String slackId; //배송담당자 슬랙 아이디
 	private UUID receiveId; //슬랙을 받을 배송담당자 아이디
 	private UUID orderId;           // 주문 아이디
-	private String customerName;       // 주문자 이름
 	private String productName;        // 상품명
 	private String orderRequest;     // 요청 사항
 	private String departHub;    // 출발 허브
